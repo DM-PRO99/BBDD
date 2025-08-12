080f35dd597cf1e1aa193d76edae133d9f635577
Diego ALejandro Mena Ciceri <diegomena903@gmail.com>
2025-08-11 23:06:28 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,16 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const express = require("express");
+const app = express();
+const mysql = require("mysql");
+const cors = require("cors");
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+app.use(cors());
+app.use(express.json());
+
+
+const db = mysql.createConnection({ 
+    host:"localhost",
+    user: "root",
+    password: "",
+    database: "empleados_crud"
+});
+
+app.post("/create",(req,res)=>{
+    const nombre =req.body.nombre;
+    const edad =req.body.edad;
+    const país =req.body.país;
+    const cargo =req.body.cargo;
+    const anios =req.body.anios;
+
+    db.query("INSERT INTO empleados(nombre,edad,país.cargo,anios) VALUES (?,?,?,?,?)",[nombre,edad,país,cargo,anios]
+    (err,result)=>{
+        if(err){
+            console.log(err);
+        }else {
+            res.send("Empleado registrado con éxito!!");
+        }
+    }
+    );
+});
+
+
+app.get("/empleados",(req,res)=>{
+    db.query("SELECT * FROM empleados",
+    (err,result)=>{
+        if(err){
+            console.log(err);
+        }else {
+            res.send(result);
+        }
+    }
+    );
+});
+
+
+app.listen(3001,()=>{
+    console.log("Corriendo en el puerto 3001")
+})
